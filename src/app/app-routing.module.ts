e2374923639f20245c4c0e6e42e5a5a108ea03f1
shashvat42200 <shashvat42200@gmail.com>
2023-05-18 17:07:13 -0700

    Fixed upper nav bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from "./components/home/home.component";
 import { AuthGuard } from "./user-state/guard/auth.guard";
 
 const routes: Routes = [
+  { path: "", redirectTo: "/login", pathMatch: "full" },
   { path: "login", component: LoginComponent },
   { path: "home", component: HomeComponent, canActivate: [AuthGuard] },
 ];
